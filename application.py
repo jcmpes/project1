c62b714a72c75e6239c0502adb998d6d0653ede6
jc <jcmpes@gmail.com>
2020-05-06 19:31:27 +0200

    First commit, register works and login does not break

---
+++ application.py
@@ -0,0 +1,116 @@
+import os
+import re
+
+from flask import Flask, flash, session, request, render_template, redirect
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import password_check, login_required
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+@login_required
+def index():
+    return "TODO"
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        hash = generate_password_hash(password)
+
+        if not username:
+            flash("No username", "warning")
+            return render_template("register.html")
+
+        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username }).rowcount != 0:
+            flash("User already exists", "warning")
+            return render_template("register.html")
+
+        if password != confirmation:
+            flash("Passwords do not match", "warning")
+            return render_template("register.html")
+
+        if len(password) < 6:
+            flash("Password is too short", "warning")
+            return render_template("register.html")
+
+        else:
+            db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
+                        {"username": username, "hash": hash})
+            db.commit()
+            flash(f"You have just registered the user '{username}'", "success")
+            return redirect("/login")
+
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Ensure username was submitted
+        if not username:
+            flash("Please provide a username", "danger")
+            return redirect("/login")
+
+        # Ensure password was submitted
+        if not password:
+            flash("Password was left blank", "danger")
+            return redirect("/login")
+
+        # Query database for username
+        row = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username": username}).fetchone()
+
+        # Ensure username exists and password is correct
+        if row is None:
+            flash("Invalid username and/or password", "danger")
+            return redirect("/login")
+
+        if not check_password_hash(row["hash"], password):
+            flash("Invalid username and/or password", "danger")
+            return redirect("/login")
+
+        # Remember which user has logged in
+        session["user_id"] = row["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+
+
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
---
+++ helpers.py
@@ -0,0 +1,79 @@
+import hashlib, binascii, os
+
+from functools import wraps
+from flask import session, redirect
+
+def hash_password(password):
+    """Hash a password for storing."""
+    """https://www.vitoshacademy.com/hashing-passwords-in-python/"""
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+
+
+def verify_password(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512',
+                                  provided_password.encode('utf-8'),
+                                  salt.encode('ascii'),
+                                  100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+
+def password_check(password):
+    """
+    Verify the strength of 'password'
+    Returns a dict indicating the wrong criteria
+    A password is considered strong if:
+        8 characters length or more
+        1 digit or more
+        1 symbol or more
+        1 uppercase letter or more
+        1 lowercase letter or more
+    """
+
+    # calculating the length
+    length_error = len(password) < 8
+
+    # searching for digits
+    digit_error = re.search(r"\d", password) is None
+
+    # searching for uppercase
+    uppercase_error = re.search(r"[A-Z]", password) is None
+
+    # searching for lowercase
+    lowercase_error = re.search(r"[a-z]", password) is None
+
+    # searching for symbols
+    symbol_error = re.search(r"\W", password) is None
+
+    # overall result
+    password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
+
+    return {
+        'password_ok' : password_ok,
+        'length_error' : length_error,
+        'digit_error' : digit_error,
+        'uppercase_error' : uppercase_error,
+        'lowercase_error' : lowercase_error,
+        'symbol_error' : symbol_error,
+    }
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
